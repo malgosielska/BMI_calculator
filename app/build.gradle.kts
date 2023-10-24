4ba7c1ebd0e4f6521315afae66f82dc404012cc4
malgosielska <malgosielska@hmail.com>
2023-10-24 20:03:35 +0200

    extract colors

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.bmi_calculator"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.bmi_calculator"
